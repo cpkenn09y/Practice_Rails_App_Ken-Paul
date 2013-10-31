e7db687aea500f71830b8311c26250198ffc5fc2
Kenneth Yu <KennethYu@Kenneths-MacBook-Pro.local>
2013-10-30 23:58:51 -0700

    added javascript files missing from previous commit

---
+++ app/assets/javascripts/jeffrey.js
@@ -0,0 +1,7 @@
+var something = new Something("mom");
+
+
+//$(function() { // shortcut!
+$(document).ready(function() {
+  alert("Hi, " + something.me + "!");
+});
---
+++ app/assets/javascripts/something.js
@@ -0,0 +1,3 @@
+var Something = function(me) {
+  this.me = me;
+}
