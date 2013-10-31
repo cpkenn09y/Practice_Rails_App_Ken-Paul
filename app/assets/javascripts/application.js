9f8d61bdcf6a9a7c6954bb6abaa3d514a1ea06b9
Kenneth Yu <KennethYu@Kenneths-MacBook-Pro.local>
2013-10-30 23:57:59 -0700

    learned about manually creating routes, nested resources, link_to helpers, form helpers, and requiring javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require something
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,30 @@
 HackerNewsRails::Application.routes.draw do
 
   root :to => 'hackers#index'
-  resources :hackers
-  # post 'users' => "hackers#create"
 
-  # post 'comments' => 'comments#create'
-  # delete 'users' => 'hackers#delete'
+  #resources :hackers
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  # ... shortcut for ...
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  get "/hackers", to: "hackers#index", as: :hackers
+  post "/hackers", to: "hackers#create"
+  get "/hackers/new", to: "hackers#new", as: :new_hacker
+  get "/hackers/:id/edit", to: "hackers#edit", as: :edit_hacker
+  get "/hackers/:id", to: "hackers#show", as: :hacker
+  put "/hackers/:id", to: "hackers#update"
+  #delete "/hackers/:id", to: "hackers#destroy"
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  # ... shortcut for ...
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  match "/hackers/:id", to: "hackers#destroy", via: [:delete]
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # ... custom routes are okay, too!
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  match "/xxx", to: "hackers#index", as: :porno, via: [:get, :put, :delete]
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+  resources :bonobos do
+    resources :eyes
+    resource :tail
+  end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
